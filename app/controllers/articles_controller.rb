b76ea3c336c6270589e376b70676d408db60a9ab
kjmosier <keithjmosier@gmail.com>
2017-07-12 11:27:51 -0700

    Add Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,10 +19,21 @@ class ArticlesController < ApplicationController
         redirect_to article_path(@article)
     end
 
-    def destroy
-      @article = Article.find(params[:id])
-      @article.destroy
+    def edit
+        @article = Article.find(params[:id])
+    end
 
-      redirect_to articles_path
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
 end
