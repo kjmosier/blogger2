0f5e4e422d4db207af99780bd08f84da4a3e76a2
kjmosier <keithjmosier@gmail.com>
2017-07-14 13:06:04 -0700

    I4 Add Photo

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,9 @@
 module ArticlesHelper
 
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
-  end
+   params.require(:article).permit(:title, :body, :tag_list, :image)
+ end
+
 
 
 end
